e8d756972fc4e0f57541eb86368bb4d97e5eefbb
shubhamk-coder <shubhamkamble8983@gmail.com>
2022-07-05 11:21:54 +0530

    if-else task completed

---
+++ javascript/if-else-task.js
@@ -0,0 +1,80 @@
+// Question 1
+/* let length = parseInt(prompt("enter length of rectangle"));
+let breadth = parseInt(prompt("enter breadth of rectangle"));
+if ( length === breadth) {
+  console.log("It is a square");
+} else {
+  console.log("It is not a square");
+} */
+
+// Question 2
+/* let num1 = parseInt(prompt("enter first number"));
+let num2 = parseInt(prompt("enter second number"));
+if (num1 > num2){
+  console.log(`${num1} is greater than ${num2}`);
+} else {
+  console.log(`${num2} is greater than ${num1}`);
+} */
+
+// Question 3
+/* let qty = parseInt(prompt("Enter Quantity"));
+cost = qty * 100;
+if (qty > 1000) {
+  let totalCost = cost - (cost / 100 * 10);
+  console.log(`total cost is ${totalCost}`);
+} else {
+  console.log(`total cost is ${cost}`);
+} */
+
+// Question 4
+/* let salary = parseInt(prompt("Enter your Salary (CTC)"));
+let service = parseInt(prompt("Enter your years of service"));
+if (service > 5) {
+  bonus = salary /100 * 5;
+  console.log(`your net bonus amount is ${bonus}`);
+} else {
+  console.log("you're not eligible for bonus")
+} */
+
+// Question 5
+/* let num = parseInt(prompt("enter any number"));
+if (num < 0) {
+            num = (-1) * num;
+        }
+console.log(`Absolute value of given number is ${num}`); */
+
+// Question 6
+/* let ch = prompt("enter a character");
+if ( ch >= "A" && ch <= "Z") {
+  console.log(ch + " is an UpperCase character ");
+}
+else if (ch >= "a" && ch <= "z") {
+  console.log(ch + " is an LowerCase character ");
+}
+else {
+  console.log(" Entered value is not a character");
+} */
+
+// Question 7
+/* let classes = parseInt(prompt("Number of classes held"));
+let attend = parseInt(prompt("Number of classes Attended"));
+let attendance = attend / classes * 100;
+console.log(`your attandance is ${attendance}%`);
+if (attendance >= 75) {
+  console.log("Student is allowed to sit in the exams");
+} else {
+  console.log("Student is not allowed to sit in the exams");
+} */
+
+// Question 8
+/* let classes = parseInt(prompt("Number of classes held"));
+let attend = parseInt(prompt("Number of classes Attended"));
+let medical = prompt("Did you have any medical cause? enter 'Y' for yes & 'N' for no");
+let attendance = attend / classes * 100;
+console.log(`your attandance is ${attendance}%`);
+if (attendance >= 75 || medical === 'Y') {
+  console.log("Student is allowed to sit in the exams");
+} else {
+  console.log("Student is not allowed to sit in the exams");
+}  */
+
