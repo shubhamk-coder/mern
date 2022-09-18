61c840cd53233c4cf8c0253982c3dc1d1028897a
shubhamk-coder <shubhamkamble8983@gmail.com>
2022-09-18 18:50:46 +0530

    Completed Image Slider task

---
+++ back-end/express/routes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+const port = 8900;
+
+// Defining the endpoint
+app.get("/", (req, res) => {
+  res.send("Hello World from Express server!");
+});
+
+app.get("/home", (req, res) => {
+  res.send("Home page");
+});
+
+app.post("/home", (req, res) => {
+  res.send("Home page via POST");
+});
+
+app.get("/filter", (req, res) => {
+  res.send("Filter page");
+});
+
+app.get("/details", (req, res) => {
+  res.send("Details page");
+});
+
+app.get("/bio", (req, res) => {
+  res.send("Bio page");
+});
+
+app.get("/support", (req, res) => {
+  res.send("Supports page");
+});
+
+// Starting the server
+app.listen(port, () => {
+  console.log(`HelloWorld app listening on port ${port}!`);
+});
---
+++ front-end/react/image-slider-task/src/components/Data.js
@@ -0,0 +1,29 @@
+const Data = [
+  {
+    id: 1,
+    title: "Lorem ipsum 1",
+    subTitle: "Lorem 1",
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum 2",
+    subTitle: "Lorem 2",
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum 3",
+    subTitle: "Lorem 3",
+  },
+  {
+    id: 4,
+    title: "Lorem ipsum 4",
+    subTitle: "Lorem 4",
+  },
+  {
+    id: 5,
+    title: "Lorem ipsum 5",
+    subTitle: "Lorem 5",
+  },
+];
+
+export default Data;
